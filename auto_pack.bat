9bdacea57856cc0332b2177ca444ad940c5d90b8
laijingfeng <337292616@qq.com>
2016-01-22 14:43:11 +0800

    增加自动打包脚本

---
+++ auto_pack.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set /p IsHome=是否家里环境?(y/n)
+if /i %IsHome% EQU y (
+	set UNITY_PATH="E:\Program Files\Unity4.6.0\Editor\"
+	set PROJECT_PATH="D:\GitHub\explorer_client"
+) else (
+	set UNITY_PATH="D:\Program Files\Unity3D\Editor"
+	set PROJECT_PATH="D:\GitHub\explorer_client"
+)
+
+cd /D %UNITY_PATH%
+::/D是跨磁盘
+
+echo Packing...
+
+start /wait Unity.exe -projectPath %PROJECT_PATH% -executeMethod Pack.Build -quit -batchmode
+
+echo Finish.
+
+pause
